070377d7cb3ba210775c325f801f41a8c6fe7df8
Galexant <galexant@yandex.ru>
2014-01-07 01:51:00 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Innovo::Application.config.secret_token = 'af9acd621a5adce703066fe62a5a9dcdc8cfd1c01af96edd3b056a56b12edce133e46ba02d2eb6aa1b77aed86e218b11015717e3fa5c0cce2d0e9959af0fdcd5'
