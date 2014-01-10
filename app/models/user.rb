b4c800c78c0fddc1b177b5c7f69e5931bfdfe7ed
Galexant <galexant@yandex.ru>
2014-01-10 10:10:00 +0400

    second commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_and_belongs_to_many :roles
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  def role?(role)
+    return !!self.roles.find_by_name(role.to_s.camelize)
+  end
+
+end
